b82eb4cb2d7633f12f6b741b17b50e8f7473aaba
aimerpante <aimerpante071102@gmail.com>
2024-11-20 11:03:04 +0800

    added database connection

--- functions.php
+++ functions.php
@@ -1,3 +1,29 @@
-<?php    
-    // All project functions should be placed here
+<?php 
+
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+
+function database_connection() {
+    $db_config = [
+        'host' => 'localhost',
+        'user' => 'root',
+        'password' => '',
+        'database' => 'dct-ccs-finals'
+    ];
+
+    $connection = new mysqli(
+        $db_config['host'], 
+        $db_config['user'], 
+        $db_config['password'], 
+        $db_config['database']
+    );
+
+    if ($connection->connect_error) {
+        error_log("Connection failed: " . $connection->connect_error, 3, '/var/log/db_errors.log');
+        return null;
+    }
+
+    return $connection;
+}
 ?>
